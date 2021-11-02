0497ffe0694a845efce2164945eec2df453cad5d
Zack <zackwilk21@gmail.com>
2021-11-01 22:01:07 -0400

    yes

--- script.js
+++ script.js
@@ -55,9 +55,3 @@ $(document).ready(function(){
 	
 	
 });
-
-$('a.yourlink').click(function(e) {
-  e.preventDefault();
-  window.open('http://yoururl1.com');
-  window.open('http://yoururl2.com');
-});
