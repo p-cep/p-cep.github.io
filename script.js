14a632346925a514b436db03f664a8722532368b
Zack <zackwilk21@gmail.com>
2021-11-02 21:02:42 -0400

    yes

--- script.js
+++ script.js
@@ -72,19 +72,11 @@ let visits = 0;
 }*/
   
 function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
+var cookies = document.cookie
+  .split(';')
+  .map(cookie => cookie.split('='))
+  .reduce((accumulator, [key, value]) => ({ ...accumulator, [key.trim()]: decodeURIComponent(value) }), {});
+  return cookies.visits;
 }
   
 function checkCookie() {
